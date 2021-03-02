deca909f6e7d2690555aeeea25da41192ec8ef02
moons14 <moons.music.mail@gmail.com>
2021-03-02 16:51:33 +0900

    Update api.php

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 $front=$_GET['front'];
 $back=$_GET['back'];
 $result=$front+$back;
-$response['status']="true";
 $response['result']=$result;
+$response['status']="true";
 $response['formula']=$front.'+'.$back;
 print json_encode($response,  JSON_UNESCAPED_UNICODE);
